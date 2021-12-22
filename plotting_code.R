3fef840affe059aa4fb1d268537b0c349a34b65c
Usama Ikram <80431533+usamaikram143@users.noreply.github.com>
2021-12-21 23:27:17 -0500

    Update plotting_code.R
    Found Mean, Median, Variance and Standard Deviation

--- plotting_code.R
+++ plotting_code.R
@@ -16,5 +16,31 @@ hist(Daily_Attendance$Released,breaks = 100,
 #as the bars after the first few bars are so small compared to the first bars and
 #without the limit it's really hard to see them.
 
+#finding mean median variance and standard deviation
+mean(Daily_Attendance$Enrolled)
+median(Daily_Attendance$Enrolled)
+var(Daily_Attendance$Enrolled)
+sd(Daily_Attendance$Enrolled)
 
+mean(Daily_Attendance$Absent)
+median(Daily_Attendance$Absent)
+var(Daily_Attendance$Absent)
+sd(Daily_Attendance$Absent)
+
+mean(Daily_Attendance$Present)
+median(Daily_Attendance$Present)
+var(Daily_Attendance$Present)
+sd(Daily_Attendance$Present)
+
+#plotting
+plot(Daily_Attendance$Present,Daily_Attendance$Absent,
+     main = "Plot against Present and Absent",
+     xlab = "Present",ylab = "Absent")
+
+#correlation
+cor(Daily_Attendance$Present,Daily_Attendance$Absent)
+
+#95% confidence interval
+ls(t.test(Daily_Attendance$Present))
+t.test(Daily_Attendance$Present)$"conf.int"
 
